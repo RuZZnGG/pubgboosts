78d6d892332baa4a8623f91a09ea5fba3396dc02
RuZZn <ruzzngg@gmail.com>
2023-01-26 02:26:22 +0300

    test

---
+++ src/main/java/net/ruzzn/pubgboosts/PUBGBoostsClient.java
@@ -0,0 +1,10 @@
+package net.ruzzn.pubgboosts;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PUBGBoostsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
